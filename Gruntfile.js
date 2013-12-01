e6bf98d9d92dd776a0c30ca5d6228f19055970b3
Szabó Gábor <szabogabi@gmail.com>
2013-12-02 00:13:15 +0100

    first commit

---
+++ Gruntfile.js
@@ -0,0 +1,113 @@
+'use strict';
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    sass: {
+      dist: {
+        options: {
+         style: 'nested',
+         sourcemap: true
+        },
+        files: {
+          'assets/css/main.min.css': 'assets/scss/main.scss'
+        }
+      }
+    },
+    autoprefixer: {
+      dist: {
+        files: {
+          'assets/css/main.min.css': 'assets/css/main.min.css'
+        }
+      }
+    },
+    jshint: {
+      options: {
+        jshintrc: '.jshintrc'
+      },
+      all: [
+        'Gruntfile.js',
+        'assets/js/*.js',
+        '!assets/js/scripts.min.js'
+      ]
+    },
+    uglify: {
+      dist: {
+        files: {
+          'assets/js/scripts.min.js': [
+            'assets/js/plugins/*.js',
+            'assets/js/_*.js'
+          ]
+        },
+        options: {
+          // JS source map: to enable, uncomment the lines below and update sourceMappingURL based on your install
+          // sourceMap: 'assets/js/scripts.min.js.map',
+          // sourceMappingURL: '/app/themes/roots/assets/js/scripts.min.js.map'
+        }
+      }
+    },
+    version: {
+      options: {
+        file: 'lib/scripts.php',
+        css: 'assets/css/main.min.css',
+        cssHandle: 'roots_main',
+        js: 'assets/js/scripts.min.js',
+        jsHandle: 'roots_scripts'
+      }
+    },
+    watch: {
+      sass: {
+          files: ['assets/scss/*.scss'],
+          tasks: ['sass', /*'autoprefixer',*/ 'version']
+      },
+      js: {
+        files: [
+          '<%= jshint.all %>'
+        ],
+        tasks: ['jshint', 'uglify', 'version']
+      },
+      livereload: {
+        // Browser live reloading
+        // https://github.com/gruntjs/grunt-contrib-watch#live-reloading
+        options: {
+          livereload: true
+        },
+        files: [
+          'assets/css/main.min.css',
+          'assets/js/scripts.min.js',
+          'templates/*.php',
+          '*.php'
+        ]
+      }
+    },
+    clean: {
+      dist: [
+        'assets/css/main.min.css',
+        'assets/js/scripts.min.js'
+      ]
+    }
+  });
+
+  // Load tasks
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-wp-version');
+  //grunt.loadNpmTasks('grunt-sass');
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-autoprefixer');
+
+
+  // Register tasks
+  grunt.registerTask('default', [
+    'clean',
+    'sass',
+    'autoprefixer',
+    'uglify',
+    'version'
+  ]);
+  grunt.registerTask('dev', [
+    'watch'
+  ]);
+
+};
